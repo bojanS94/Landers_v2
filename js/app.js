15b560604f392d1fb88e3be8044fc468a484de7c
bojanS94 <eastcodewestcode94@gmail.com>
2021-01-01 18:03:29 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
-const slides = document.querySelectorAll(".flow_div");
-const next = document.querySelector("#next");
-const prev = document.querySelector("#prev");
+$(document).ready(function () {
+    $('#next').on('click', function () {
+        var currentImg = $('.current');
+        var nextImg = currentImg.next();
+
+        if (nextImg.length) {
+
+            $(currentImg).fadeOut('fast', function () {
+
+                $(this).removeClass('current');
+
+            });
+
+            $(nextImg).fadeIn('fast', function () {
+
+                $(this).addClass('current');
+
+            });
+        }
+    });
+
+    $('#prev').on('click', function () {
+        var currentImg = $('.current');
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            $(currentImg).fadeOut('fast', function () {
+
+                $(this).removeClass('current');
+
+            });
+
+            $(prevImg).fadeIn('fast', function () {
+
+                $(this).addClass('current');
+
+            });
+        }
+    });
+});
